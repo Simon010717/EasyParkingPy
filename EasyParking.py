800738b00d715f36dfb3ba65a1de91168bccc566
Simon <apariciosimon@gmail.com>
2019-11-03 17:56:33 -0500

    Persistencia Archivos

--- EasyParking.py
+++ EasyParking.py
@@ -1,4 +1,5 @@
-import time, random, string
+import time, random, string, os, time
+import data_structures as dt
 
 
 class Carro:
@@ -21,35 +22,109 @@ class Persona:
 class Usuario(Persona):
     def __init__(self, ced, nickname, password, nombre, apellido, edad, placa, email=None, direccion=None, tel=None):
         self.carro = Carro(placa)
-        Persona.__init__(self, ced, nickname, password, nombre, apellido, edad, email, direccion, tel)
+        super().__init__(ced, nickname, password, nombre, apellido, edad, email, direccion, tel)
 
 class Espacio:
-    def __init__ (self,numero):
-        self.numero = numero
-        libre = True
-        carro = None
+    def __init__ (self,cod,hora_incio = None,libre = True,carro = None):
+        self.cod = cod
+        self.hora_incio = hora_incio
+        self.libre = libre
+        self.carro = carro
 
-    def llenar(self,usuario):
-        self.carro = usuario.carro
-        self.libre = False
+class EasyParking:
 
-class Parqueadero:
     def __init__(self):
-        self.espacios = []
+        self.usuariosRoute = "usuarios.ep"
+        self.parqueaderosRoute = "parqueaderos"
         self.usuarios = []
-        self.espaciosTotales = 0
-        self.espaciosOcupados = 0
+        self.addUsuarios()
+        self.parqueaderos = []
+        self.addParqueaderos()
 
-    def addEspacio(self):
-        self.espacios.append(Espacio(self.espaciosTotales+1))
+    def addParqueaderos(self):
+        n = len(os.listdir(self.parqueaderosRoute))
+        for i in range(n):
+            with open(self.parqueaderosRoute+"/p"+str(i),"r") as f:
+                data = f.readlines()
+            l = 0
+            data[l] = data[l].rstrip('\n')
+            self.addParqueadero(data[l].split('*'),True)
+            l += 1
+            ocupados = []
+            for b in range (len(data[l])):
+                if data[l][b] == '1': ocupados.append(b)
+            l += 1
+            for p in ocupados:
+                info = data[l].split('*')
+                info[1] = info[1].rstrip('\n')
+                self.parqueaderos[i].espacios[p] = Espacio(l-2,info[0],self.buscarUsuario(info[1]).carro,False)
+                l+=1
+    
+    def buscarUsuario(self,ced):
+        for u in self.usuarios:
+            if ced == u.ced: return u
+        return None
+        
+    def addParqueadero(self,info,verified):
+        if not verified:
+            for p in self.parqueaderos:
+                if p.cod == info[1]: return False
+            data = []
+            data.append('*'.join(info)+'\n')
+            data.append("0"*(int(info[5])-int(info[6])))
+            print(data)
+            with open(self.parqueaderosRoute+"/p"+str(len(self.parqueaderos)),"w") as f:
+                f.writelines(data)
+        self.parqueaderos.append(Parqueadero(info[0],info[1],info[2],info[3],info[4],int(info[5]),int(info[6])))
+        
+    def addUsuarios(self):
+        with open(self.usuariosRoute,"r") as f:
+            data = f.readlines()
+        
+        for line in data:
+            self.addUsuarioArr(line.split("*"),True)
 
-    def addUsuario(self, ced, nickname, password, nombre, apellido, edad, placa, email=None, direccion=None, tel=None):
-        self.usuarios.append(Usuario(ced, nickname, password, nombre, apellido, edad, placa, email, direccion, tel))
+    def addUsuarioArr(self,info,verified):
+        if len(info) < 7: return None
+        if not verified:
+            check = self.checkInfo(info)
+            if check < 3: return check
 
-    def buscarId(self,id):
-        for i in range(len(self.usuarios)):
-            if(self.usuarios[i].id == id):
-                return usuarios[i]
+        line = '*'.join(info)
+
+        for i in range(len(info),10):
+            info.append(None)
+
+        self.usuarios.append(Usuario(info[0],info[1],info[2],info[3],info[4],info[5],info[6],info[7],info[8],info[9]))
+        
+        if not verified:
+            with open(self.usuariosRoute,"a") as f:
+                if self.usuarios > 1: f.write('\n')
+                f.writelines(line)
+
+        return 3
+    
+    def checkLogin(self,nickname,password):
+        for u in self.usuarios:
+            if u.nickname == nickname:
+                if u.password == password: return True
+                return False
+        return False
+
+
+    #def addUsuario(self, ced, nickname, password, nombre, apellido, edad, placa, email=None, direccion=None, tel=None):
+    #    self.usuarios.append(Usuario(ced, nickname, password, nombre, apellido, edad, placa, email, direccion, tel))
+
+    def checkInfo(self,info):
+        ced = info[0]
+        nickname = info[1]
+        placa = info[2]
+
+        for u in self.usuarios:
+            if u.ced == ced: return 0
+            if u.nickname == nickname: return 1
+            if u.carro.placa == placa: return 2
+        return 3
     
     def comprobarUsuario(self, nickname, password):
         for i in range(len(self.usuarios)):
@@ -58,11 +133,29 @@ class Parqueadero:
         
         return False
     
+class Parqueadero:
+
+    def __init__(self,nombre,cod,direccion,tel,gerente,totales,ocupados):
+        self.espaciosTree = dt.AvlTree()
+        self.espacios = [None]*totales
+        self.totales = totales
+        self.ocupados = ocupados
+
+        self.nombre = nombre
+        self.cod = cod
+        self.direccion = direccion
+        self.tel = tel
+        self.gerente = gerente
+            
+
+    def addEspacio(self):
+        self.espacios.append(Espacio(self.espaciosTotales+1))
+    
     def puestosVacios(self):
         return self.espaciosTotales - len(self.espacios)
     
     def nextLibre(self):
-        if(len(self.espacios) < espaciosTotales):
+        if(len(self.espacios) < totales):
             if(len(self.espacios) == 0): 
                 return 0
             elif (self.espacios[len(self.espacios)-1].numero < espaciosTotales-1):
@@ -107,15 +200,16 @@ class Test:
 
 
 def main():
-    print("Testing")
-    user = Usuario("1001","saparicio","SAB","Simon","Aparicio",15,"DNS243")
-    print(user.nombre)
+    ep = EasyParking()
+    info = ["0000011111","nona","passa","angela","houston","26","tyh529"]
+    ep.addUsuarioArr(info,False)
+    info = ["apestas","12345","aval","123456","tomas","5","0"]
+    ep.addParqueadero(info,False)
 
 
 
 if __name__ == "__main__":
-    test = Test()
-    print(test.testDynamicArray(1000000,500000))
+    main()
 
 
     
